c4f1c446186ed2feb47c5f6eb73e98b0bc0eceb4
Mustain <kero2.gunzo@gmail.com>
2022-03-09 04:26:24 +0700

    Update Montok - Dummy Data.js

--- Snippets/Montok - Dummy Data.js
+++ Snippets/Montok - Dummy Data.js
@@ -1,2 +1,2 @@
 $('.button-bot-status .status').addClass('status-success').addClass('blink')
-$('.button-bot-status span:last-child').html('Bot Aktif')
+$('.button-bot-status span:last-child').html('    Bot Aktif    ')
